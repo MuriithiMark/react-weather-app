10bcca187ce4a08b8c0526c5a488d8e6f32b73a3
MuriithiMark <markmwangi14317@gmail.com>
2024-01-27 01:21:02 +0300

    updated api key

--- src/services/api.js
+++ src/services/api.js
@@ -1,11 +1,6 @@
-// const OPEN_WEATHER_API_KEY = "c2968e404b7188d365a90cc1a7c10780";
-// const CORS_ANYWHERE_URL = "http://localhost:8001/"
-const API_KEY = "ae2a0b0783c26f2da1386f42ae36fb88";
+const API_KEY = "c2968e404b7188d365a90cc1a7c10780";
 
-// lat={lat}&lon={lon}
-// lat=${lat}&lon=${lon}
 async function getCurrentWeatherData(cityName) {
-  // const [lat, lon]= await getCoordinates(cityName)
   const url = `http://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${API_KEY}`;
   const response = await fetch(url);
   const data = await response.json();
